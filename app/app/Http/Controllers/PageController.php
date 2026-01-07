143903841aab64f3d21b4a2d470625d928aed74f
Sebastian Muñoz Riquelme <112529007+JanBoseyur@users.noreply.github.com>
2026-01-06 22:03:56 -0300

    Vista Login-Register

---
+++ app/app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function AdminDashboard()
+    {
+        return view('admin.AdminDashboard');
+    }
+}
---
+++ app/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/login', [PageController::class, 'login'])->name('login');
+Route::get('/register', [PageController::class, 'register'])->name('register');
+Route::get('/home', [PageController::class, 'home'])->name('home');
+Route::get('/AdminDashboard', [PageController::class, 'AdminDashboard'])->name('AdminDashboard');
